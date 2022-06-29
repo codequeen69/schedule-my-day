2c08a9864b397e826e67ba58a83b38767f9ad8f5
Venus DM <crhowe90@gmail.com>
2022-06-29 09:22:15 -0500

    save to local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+var hourEvent;
+
 //Display current date
 $("#currentDay").text(moment().format("MMMM Do YYYY"));
 
+
+//save event to local storage
+var saveHourEvent = function (){
+  var saveText = $(this).siblings(".description").val();
+    var saveHour = $(this).siblings(".hour").text();
+   localStorage.setItem(saveHour, saveText)
+}
+var getHourEvent = function(){
+  var textEvent = localStorage.getItem("9AM")
+$("#hour-text-9").val(textEvent);
+localStorage.getItem("10AM")
+$("#hour-text-10").val()
+
+}
+getHourEvent();
+$(".saveBtn").on("click", saveHourEvent)
