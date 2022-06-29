34978b8a3be6291dfef6dee4322d261337bfe188
Venus DM <crhowe90@gmail.com>
2022-06-29 17:26:34 -0500

    clean up

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,7 @@
 var hourEvent = document.querySelector(".description");
 var currentTime = moment().hour();
 
+
 //Display current date
 $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm a"));
 
@@ -22,84 +23,72 @@ $("#hour-text-15").val(localStorage.getItem("3PM"));
 $("#hour-text-16").val(localStorage.getItem("4PM"));
 $("#hour-text-17").val(localStorage.getItem("5PM"));
 
-var timeOfDay = function () {
   //set hour for corresponding text area
-  hourNine = moment().hour(12).format('H');
-  hourTen = moment().hour(12).format('H');
-  hourEleven = moment().hour(12).format('H');
+  hourNine = moment().hour(9).format('H');
+  hourTen = moment().hour(10).format('H');
+  hourEleven = moment().hour(11).format('H');
   hourTwelve = moment().hour(12).format('H');
   hourThirteen = moment().hour(13).format('H');
   hourFourteen = moment().hour(14).format('H');
   hourFifteen = moment().hour(15).format('H');
-  hourSixteen = moment().hour(15).format('H');
+  hourSixteen = moment().hour(16).format('H');
   hourSeventeen = moment().hour(17).format('H');
   //set background color depending on hour of day
   if (hourNine == currentTime) {
-    $("#hour-text-9").removeClass("past");
     $("#hour-text-9").addClass("present");
   }
   if (hourNine > currentTime) {
     $("#hour-text-9").addClass("future");
   }
   if (hourTen == currentTime) {
-    $("#hour-text-10").removeClass("past");
     $("#hour-text-10").addClass("present");
   }
   if (hourTen > currentTime) {
     $("#hour-text-10").addClass("future");
   }
   if (hourEleven == currentTime) {
-    $("#hour-text-11").removeClass("past");
     $("#hour-text-11").addClass("present");
   }
   if (hourEleven > currentTime) {
     $("#hour-text-11").addClass("future");
   }
   if (hourTwelve == currentTime) {
-    $("#hour-text-12").removeClass("past");
     $("#hour-text-12").addClass("present");
   }
   if (hourTwelve > currentTime) {
     $("#hour-text-12").addClass("future");
   }
   if (hourThirteen == currentTime) {
-    $("#hour-text-13").removeClass("past");
     $("#hour-text-13").addClass("present");
   }
   if (hourThirteen > currentTime) {
-    $("#hour-text-13").removeClass("past");
     $("#hour-text-13").addClass("future");
   }
   if (hourFourteen == currentTime) {
-    $("#hour-text-14").removeClass("past");
     $("#hour-text-14").addClass("present");
   }
   if (hourFourteen > currentTime) {
     $("#hour-text-14").addClass("future");
   }
   if (hourFifteen == currentTime) {
-    $("#hour-text-15").removeClass("past");
     $("#hour-text-15").addClass("present");
   }
   if (hourFifteen > currentTime) {
     $("#hour-text-15").addClass("future");
   }
   if (hourSixteen == currentTime) {
-    $("#hour-text-16").removeClass("past");
     $("#hour-text-16").addClass("present");
   }
   if (hourSixteen > currentTime) {
     $("#hour-text-16").addClass("future");
   }
   if (hourSeventeen == currentTime) {
-    $("#hour-text-17").removeClass("past");
     $("#hour-text-17").addClass("present");
   }
   if (hourSeventeen > currentTime) {
     $("#hour-text-17").addClass("future");
   }
-};
-timeOfDay();
+
 
 
 $(".saveBtn").on("click", saveHourEvent)
